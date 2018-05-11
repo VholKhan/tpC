2e100560426d3cf2be9965bef286020db667d68d
Voxad <apj@SOMONE-APJ.localdomain>
2018-05-11 10:16:23 +0200

    first test commit

--- tabDyn.h
+++ tabDyn.h
@@ -3,22 +3,22 @@
 #ifndef  __TAB_DYN__      // afin d'eviter les doubles declarations
 #define  __TAB_DYN__
 
-typedef  int  myType ;
+typedef  int  dynTab ;
 
 /* --- L'init. Il faut créé dynamiquement un tableau de SIZE entiers --- */
-myType *  construct( ) ;
+dynTab *  construct( ) ;
 /* --- Detruire le tableau --- */
-void   destructeur( myType * ) ;
+void   kill( dynTab * ) ;
 /* --- Affichage du tableau --- */
-void   Affiche( myType * ) ;
+void   show( dynTab * ) ;
 /* --- Ajout une valeur. Le tableau peut etre agrandit, donc pt modifie --- */
-void   ajout( myType ** , myType ) ;
+void   add( dynTab ** , dynTab ) ;
 /* --- Rend la position ou on a ajoute une valeur --- */
 int    getFinAdd( ) ;
 /* --- rend la taille du tableau --- */
 int    getSize( ) ;
 /* --- rend la valeur à la position du tableau --- */
-myType getVal( myType *, int ) ;
+myType getVal( dynTab *, int ) ;
 /* --- rend vrai si on est a la fin du tableau --- */
 int    isEnd( ) ;
 /* --- rend vrai si on est au debut du tableau --- */
